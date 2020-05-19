e2880baa1bde2fcbb4eb2a8326449dfab221c4d1
richardbobo1 <richard.bobo@gmail.com>
2020-05-19 17:29:18 -0400

    Done.

--- sushi-saga-client/src/components/MoreButton.js
+++ sushi-saga-client/src/components/MoreButton.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const MoreButton = (props) => {
-    return <button onClick={/* Fill me in! */ null}>
+  ///why does this onclick have 'null' as a default? 
+    return <button onClick={props.morePlease}>
             More sushi!
           </button>
 }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi';
 
 const SushiContainer = (props) => {
+  let sushiIndex= props.index
+  console.log(sushiIndex) 
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+
+    {
+      props.sushis.map(sushi => <Sushi onClickToEat={props.onClickToEat} data={sushi} eaten={props.eaten.includes(sushi)} key={sushi.id} /> )
+    }
+    {/* // eaten=props.eaten.includes(sushi)  returns a boolean for use on next level  */}
+
+
+        <MoreButton morePlease={props.morePlease} />
       </div>
     </Fragment>
   )
